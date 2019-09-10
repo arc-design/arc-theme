b4865b46d515aeb30b07b70676a3f5d5516a6f60
David Mohammed <fossfreedom@ubuntu.com>
2019-09-10 21:59:10 +0100

    mkrelease script

---
+++ mkrelease.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+VERSION="20190910"
+NAME="arc-theme"
+git-archive-all.sh --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
+xz -9 "${NAME}-${VERSION}.tar"
+
+gpg --default-key 1E1FB0017C998A8AE2C498A6C2EAA8A26ADC59EE --armor --detach-sign "${NAME}-${VERSION}.tar.xz"
+gpg --verify "${NAME}-${VERSION}.tar.xz.asc"
+
